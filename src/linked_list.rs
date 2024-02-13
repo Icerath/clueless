3b74dca41e996753b2cb4e22d2884966291304d2
Dorje Gilfillan <dorjegilfillan@gmail.com>
2024-02-13 23:55:56 +0200

    linked_list: as interesting as LinkedList being generic over T was, I don't think it's worth the complexity.

--- src/linked_list.rs
+++ src/linked_list.rs
@@ -5,89 +5,40 @@ use core::fmt;
 
 use crate::vec::Vec;
 
-pub mod small {
-    #[allow(private_interfaces)]
-    pub type LinkedList<T> = super::LinkedList<T, u32>;
-}
-
-pub mod large {
-    #[allow(private_interfaces)]
-    pub type LinkedList<T> = super::LinkedList<T, usize>;
-}
-
-pub trait Index: Eq + Copy + private::Sealed {
-    const NIL: Self;
-    fn usize(self) -> usize;
-    fn from_usize(val: usize) -> Self;
-}
-
-mod private {
-    pub trait Sealed {}
-
-    impl Sealed for usize {}
-    impl Sealed for u32 {}
-}
-
-impl Index for u32 {
-    const NIL: Self = Self::MAX;
-
-    #[inline]
-    fn usize(self) -> usize {
-        self as usize
-    }
-
-    #[inline]
-    fn from_usize(val: usize) -> Self {
-        val.try_into().expect("Linked List grew too large")
-    }
-}
-
-impl Index for usize {
-    const NIL: Self = Self::MAX;
-
-    fn usize(self) -> usize {
-        self
-    }
-
-    fn from_usize(val: usize) -> Self {
-        val
-    }
-}
+type Idx = usize;
+const NIL: usize = Idx::MAX;
 
 #[derive(Clone)]
-pub struct LinkedList<T, Idx = usize> {
-    buf: Vec<Node<T, Idx>>,
+pub struct LinkedList<T> {
+    buf: Vec<Node<T>>,
     head: Idx,
     tail: Idx,
 }
 
-impl<T, Idx> LinkedList<T, Idx>
-where
-    Idx: Index,
-{
+impl<T> LinkedList<T> {
     #[must_use]
     pub const fn new() -> Self {
-        Self { buf: Vec::new(), head: Idx::NIL, tail: Idx::NIL }
+        Self { buf: Vec::new(), head: NIL, tail: NIL }
     }
 
     pub fn push_back(&mut self, val: T) {
-        let ptr = self.push_buf(Node { val, prev: self.tail, next: Idx::NIL });
-        if self.head == Idx::NIL {
+        let ptr = self.push_buf(Node { val, prev: self.tail, next: NIL });
+        if self.head == NIL {
             self.head = ptr;
         }
-        if self.tail != Idx::NIL {
-            self.buf[self.tail.usize()].next = ptr;
+        if self.tail != NIL {
+            self.buf[self.tail].next = ptr;
         }
         self.tail = ptr;
     }
 
     pub fn push_front(&mut self, val: T) {
-        let ptr = self.push_buf(Node { val, next: self.head, prev: Idx::NIL });
-        if self.tail == Idx::NIL {
+        let ptr = self.push_buf(Node { val, next: self.head, prev: NIL });
+        if self.tail == NIL {
             self.tail = ptr;
         }
-        if self.head != Idx::NIL {
-            self.buf[self.head.usize()].prev = ptr;
+        if self.head != NIL {
+            self.buf[self.head].prev = ptr;
         }
         self.head = ptr;
     }
@@ -98,8 +49,8 @@ where
         }
         let node = self.remove_node(self.tail);
         self.tail = node.prev;
-        if self.tail != Idx::NIL {
-            self.buf[self.tail.usize()].next = Idx::NIL;
+        if self.tail != NIL {
+            self.buf[self.tail].next = NIL;
         }
         Some(node.val)
     }
@@ -110,8 +61,8 @@ where
         }
         let node = self.remove_node(self.head);
         self.head = node.next;
-        if self.head != Idx::NIL {
-            self.buf[self.head.usize()].prev = Idx::NIL;
+        if self.head != NIL {
+            self.buf[self.head].prev = NIL;
         }
         Some(node.val)
     }
@@ -127,12 +78,12 @@ where
     }
 
     #[must_use]
-    pub fn iter(&self) -> Iter<'_, T, Idx> {
+    pub fn iter(&self) -> Iter<'_, T> {
         self.into_iter()
     }
 
     #[must_use]
-    pub fn iter_mut(&mut self) -> IterMut<'_, T, Idx> {
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
         self.into_iter()
     }
 
@@ -140,52 +91,46 @@ where
         self.buf.reserve(additional);
     }
 
-    fn remove_node(&mut self, ptr: Idx) -> Node<T, Idx> {
+    fn remove_node(&mut self, ptr: Idx) -> Node<T> {
         let end = &self.buf[self.len() - 1];
         let (end_prev, end_next) = (end.prev, end.next);
 
-        if let Some(prev) = self.buf.get_mut(end_prev.usize()) {
+        if let Some(prev) = self.buf.get_mut(end_prev) {
             prev.next = ptr;
         }
-        if let Some(next) = self.buf.get_mut(end_next.usize()) {
+        if let Some(next) = self.buf.get_mut(end_next) {
             next.prev = ptr;
         }
-        let node = self.buf.swap_remove(ptr.usize());
+        let node = self.buf.swap_remove(ptr);
 
-        if self.head.usize() == self.len() {
+        if self.head == self.len() {
             self.head = ptr;
         }
-        if self.tail.usize() == self.len() {
+        if self.tail == self.len() {
             self.tail = ptr;
         }
         node
     }
 
-    fn push_buf(&mut self, node: Node<T, Idx>) -> Idx {
+    fn push_buf(&mut self, node: Node<T>) -> Idx {
         let ptr = self.buf.len();
         self.buf.push(node);
-        Idx::from_usize(ptr)
+        ptr
     }
 }
 
-impl<T, Idx> Default for LinkedList<T, Idx>
-where
-    Idx: Index,
-{
+impl<T> Default for LinkedList<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
 // IntoIter
-pub struct IntoIter<T, Idx> {
-    list: LinkedList<T, Idx>,
+pub struct IntoIter<T> {
+    list: LinkedList<T>,
 }
 
-impl<T, Idx> Iterator for IntoIter<T, Idx>
-where
-    Idx: Index,
-{
+impl<T> Iterator for IntoIter<T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -201,22 +146,16 @@ where
     }
 }
 
-impl<T, Idx> DoubleEndedIterator for IntoIter<T, Idx>
-where
-    Idx: Index,
-{
+impl<T> DoubleEndedIterator for IntoIter<T> {
     fn next_back(&mut self) -> Option<Self::Item> {
         self.list.pop_back()
     }
 }
 
-impl<T, Idx> ExactSizeIterator for IntoIter<T, Idx> where Idx: Index {}
+impl<T> ExactSizeIterator for IntoIter<T> {}
 
-impl<T, Idx> IntoIterator for LinkedList<T, Idx>
-where
-    Idx: Index,
-{
-    type IntoIter = IntoIter<T, Idx>;
+impl<T> IntoIterator for LinkedList<T> {
+    type IntoIter = IntoIter<T>;
     type Item = T;
 
     fn into_iter(self) -> Self::IntoIter {
@@ -224,23 +163,20 @@ where
     }
 }
 // Iter
-pub struct Iter<'a, T, Idx> {
-    list: &'a LinkedList<T, Idx>,
+pub struct Iter<'a, T> {
+    list: &'a LinkedList<T>,
     head: Idx,
     tail: Idx,
     len: usize,
 }
 
-impl<'a, T, Idx> Iterator for Iter<'a, T, Idx>
-where
-    Idx: Index,
-{
+impl<'a, T> Iterator for Iter<'a, T> {
     type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.len = self.len.checked_sub(1)?;
-        let val = &self.list.buf[self.head.usize()].val;
-        self.head = self.list.buf[self.head.usize()].next;
+        let val = &self.list.buf[self.head].val;
+        self.head = self.list.buf[self.head].next;
         Some(val)
     }
 
@@ -253,25 +189,19 @@ where
     }
 }
 
-impl<T, Idx> DoubleEndedIterator for Iter<'_, T, Idx>
-where
-    Idx: Index,
-{
+impl<T> DoubleEndedIterator for Iter<'_, T> {
     fn next_back(&mut self) -> Option<Self::Item> {
         self.len = self.len.checked_sub(1)?;
-        let val = &self.list.buf[self.tail.usize()].val;
-        self.tail = self.list.buf[self.tail.usize()].prev;
+        let val = &self.list.buf[self.tail].val;
+        self.tail = self.list.buf[self.tail].prev;
         Some(val)
     }
 }
 
-impl<T, Idx> ExactSizeIterator for Iter<'_, T, Idx> where Idx: Index {}
+impl<T> ExactSizeIterator for Iter<'_, T> {}
 
-impl<'a, T, Idx> IntoIterator for &'a LinkedList<T, Idx>
-where
-    Idx: Index,
-{
-    type IntoIter = Iter<'a, T, Idx>;
+impl<'a, T> IntoIterator for &'a LinkedList<T> {
+    type IntoIter = Iter<'a, T>;
     type Item = &'a T;
 
     fn into_iter(self) -> Self::IntoIter {
@@ -280,24 +210,21 @@ where
 }
 
 // IterMut
-pub struct IterMut<'a, T, Idx> {
-    list: &'a mut LinkedList<T, Idx>,
+pub struct IterMut<'a, T> {
+    list: &'a mut LinkedList<T>,
     head: Idx,
     tail: Idx,
     len: usize,
 }
 
-impl<'a, T, Idx> Iterator for IterMut<'a, T, Idx>
-where
-    Idx: Index,
-{
+impl<'a, T> Iterator for IterMut<'a, T> {
     type Item = &'a mut T;
 
     fn next(&mut self) -> Option<Self::Item> {
         self.len = self.len.checked_sub(1)?;
         let head = self.head;
-        self.head = self.list.buf[self.head.usize()].next;
-        let val = &mut self.list.buf[head.usize()].val;
+        self.head = self.list.buf[self.head].next;
+        let val = &mut self.list.buf[head].val;
 
         // FIXME: remove this shit.
         #[allow(unsafe_code)]
@@ -305,15 +232,12 @@ where
     }
 }
 
-impl<'a, T, Idx> DoubleEndedIterator for IterMut<'a, T, Idx>
-where
-    Idx: Index,
-{
+impl<'a, T> DoubleEndedIterator for IterMut<'a, T> {
     fn next_back(&mut self) -> Option<Self::Item> {
         self.len = self.len.checked_sub(1)?;
         let tail = self.tail;
-        self.tail = self.list.buf[self.tail.usize()].prev;
-        let val = &mut self.list.buf[tail.usize()].val;
+        self.tail = self.list.buf[self.tail].prev;
+        let val = &mut self.list.buf[tail].val;
 
         // FIXME: remove this shit.
         #[allow(unsafe_code)]
@@ -321,11 +245,8 @@ where
     }
 }
 
-impl<'a, T, Idx> IntoIterator for &'a mut LinkedList<T, Idx>
-where
-    Idx: Index,
-{
-    type IntoIter = IterMut<'a, T, Idx>;
+impl<'a, T> IntoIterator for &'a mut LinkedList<T> {
+    type IntoIter = IterMut<'a, T>;
     type Item = &'a mut T;
 
     fn into_iter(self) -> Self::IntoIter {
@@ -333,10 +254,7 @@ where
     }
 }
 
-impl<T, Idx> FromIterator<T> for LinkedList<T, Idx>
-where
-    Idx: Index,
-{
+impl<T> FromIterator<T> for LinkedList<T> {
     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
         let iter = iter.into_iter();
         let mut list = Self::default();
@@ -348,34 +266,29 @@ where
     }
 }
 
-impl<T, Idx> LinkedList<T, Idx> {
+impl<T> LinkedList<T> {
+    #[must_use]
     pub fn into_iter_unordered(self) -> impl ExactSizeIterator + DoubleEndedIterator {
         self.buf.into_iter().map(|node| node.val)
     }
 }
 
-impl<T, Idx> LinkedList<T, Idx>
-where
-    Idx: Index,
-{
+impl<T> LinkedList<T> {
+    #[must_use]
     pub fn iter_unordered(&self) -> impl ExactSizeIterator + DoubleEndedIterator + '_ {
         self.buf.iter().map(|node| &node.val)
     }
 }
 
-impl<T, Idx> LinkedList<T, Idx>
-where
-    Idx: Index,
-{
+impl<T> LinkedList<T> {
     pub fn iter_mut_unordered(&mut self) -> impl ExactSizeIterator + DoubleEndedIterator + '_ {
         self.buf.iter_mut().map(|node| &mut node.val)
     }
 }
 
-impl<T, Idx> fmt::Debug for LinkedList<T, Idx>
+impl<T> fmt::Debug for LinkedList<T>
 where
     T: fmt::Debug,
-    Idx: Index,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_list().entries(self).finish()
@@ -383,7 +296,7 @@ where
 }
 
 #[derive(Clone)]
-pub struct Node<T, Idx> {
+pub struct Node<T> {
     val: T,
     next: Idx,
     prev: Idx,
@@ -391,7 +304,7 @@ pub struct Node<T, Idx> {
 
 #[test]
 fn test_basics() {
-    let mut list = small::LinkedList::new();
+    let mut list = LinkedList::new();
     list.push_back(1);
     list.push_back(2);
     list.push_back(3);
